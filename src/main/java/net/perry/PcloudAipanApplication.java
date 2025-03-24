9b0a8065a6443d6720ff845cbdf04e3e45b734ed
qcloud <ubuntu@localhost.localdomain>
2025-03-24 17:55:59 +0800

    first commit

---
+++ src/main/java/net/perry/PcloudAipanApplication.java
@@ -0,0 +1,13 @@
+package net.perry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcloudAipanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcloudAipanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/perry/PcloudAipanApplicationTests.java
@@ -0,0 +1,13 @@
+package net.perry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcloudAipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
